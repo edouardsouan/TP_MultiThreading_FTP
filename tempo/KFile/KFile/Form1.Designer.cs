64595661912e3dccee1b73625c545279cf1c5c52
kimsavinfo <kimsavinfo@gmail.com>
2014-12-02 04:14:41 +0100

    [ADD]
    Projet File qui permet de se connecter à un serveur FTP, testé avec mon
    serveur perso

---
+++ tempo/KFile/KFile/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace KFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtServer = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.txtusername = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.txtPassword = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.txtPort = new System.Windows.Forms.ToolStripTextBox();
+            this.btnConnect = new System.Windows.Forms.ToolStripButton();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.logWindow = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.txtServer,
+            this.toolStripLabel2,
+            this.txtusername,
+            this.toolStripLabel3,
+            this.txtPassword,
+            this.toolStripLabel4,
+            this.txtPort,
+            this.btnConnect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(714, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(48, 22);
+            this.toolStripLabel1.Text = "Server : ";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(125, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(71, 22);
+            this.toolStripLabel2.Text = "UserName : ";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(66, 22);
+            this.toolStripLabel3.Text = "Password : ";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(38, 22);
+            this.toolStripLabel4.Text = "Port : ";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 25);
+            this.txtPort.Text = "21";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnConnect.Image = global::KFile.Properties.Resources.connect_to_database;
+            this.btnConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(23, 22);
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            // 
+            // rchLog
+            // 
+            this.logWindow.Location = new System.Drawing.Point(0, 28);
+            this.logWindow.Name = "rchLog";
+            this.logWindow.Size = new System.Drawing.Size(228, 117);
+            this.logWindow.TabIndex = 1;
+            this.logWindow.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 263);
+            this.Controls.Add(this.logWindow);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtServer;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox txtusername;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripTextBox txtPassword;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripTextBox txtPort;
+        private System.Windows.Forms.ToolStripButton btnConnect;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private System.Windows.Forms.RichTextBox logWindow;
+    }
+}
+
---
+++ tempo/KFile/KFile/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KFile
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        private Byte[] Buffer = new Byte[512];
+        private int Bytes;
+        private bool Logged = false;
+        private Socket FTPSocket = null;
+        private string Password = "";
+        private int Port;
+        private string Result="";
+        private string Server = "";
+        private int StatusCode;
+        private string StatusMessage = "";
+        private string UserName = "";
+        #endregion
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            // The password characters are only asterisks
+            txtPassword.TextBox.PasswordChar = '*';
+        }
+        #endregion
+
+        #region Destructor
+        ~Form1()
+        {
+            FTPLogout();
+        }
+        #endregion
+
+        #region Buttons actions
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            Server = txtServer.Text;
+            UserName = txtusername.Text;
+            Password = txtPassword.Text;
+            Port = int.Parse(txtPort.Text);
+            FTPLogin();
+
+            // TODO : put it in the log window
+            // Alert the user for the moment
+            if (Logged)
+            {
+                MessageBox.Show("Logged !");
+            }
+            else
+            {
+                MessageBox.Show("Please check your user name and your password.");
+            }
+        }
+        #endregion
+
+        #region Handle FTP Connection
+
+        private void FTPLogin()
+        {
+            // Close the connection if one is already open
+            if (Logged)
+            {
+                CloseConnection();
+            }
+
+            // Clean IP variables
+            IPAddress remoteAddress = null;
+            IPEndPoint addrEndPoint = null;
+            
+            // Try to open the connection, can we proceed ?
+            WriteTextInLogWindow(logWindow, "Status : Opening Connection to : " + Server + "\n", Color.Red);
+            try
+            {
+                FTPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                WriteTextInLogWindow(logWindow, "Status : Resolving IP Address\n", Color.Red);
+                remoteAddress = Dns.GetHostEntry(Server).AddressList[0];
+                WriteTextInLogWindow(logWindow, "Status : IP Address Found ->" + remoteAddress.ToString() + "\n", Color.Red);
+                addrEndPoint = new IPEndPoint(remoteAddress, Port);
+                WriteTextInLogWindow(logWindow, "Status : EndPoint Found ->" + addrEndPoint.ToString() + "\n", Color.Red);
+                FTPSocket.Connect(addrEndPoint);
+
+                StartFTPConnection();
+            }
+            catch (Exception ex)
+            {
+                if (FTPSocket != null && FTPSocket.Connected)
+                {
+                    FTPSocket.Close();
+                }
+                WriteTextInLogWindow(logWindow, "Status : Couldn't connect to remote server. " + ex.Message + "\n", Color.Red);
+            }
+        }
+
+        private void StartFTPConnection()
+        {
+            ReadResponse();
+            FTPLoginHandleFTPSocketResponse();
+        }
+
+        private void FTPLoginHandleFTPSocketResponse()
+        {
+            // 220 : the server is ready for a new user
+            if (StatusCode != 220)
+            {
+                CloseConnection();
+                WriteTextInLogWindow(logWindow, "Status : " + Result.Substring(4) + "\n", Color.Red); //Error
+            }
+            else
+            {
+                FTPLoginSendUserName();
+            }
+        }
+
+        private void FTPLoginSendUserName()
+        {
+            SendCommand("USER " + UserName);
+
+            // 230 : the user is logged in, proceed
+            // 331 : the user has been recognized, waiting for the password
+            // 530 : connection fail, inform the user
+            if (!(StatusCode == 230 || StatusCode == 331) || StatusCode == 530)
+            {
+                FTPLogout();
+                WriteTextInLogWindow(logWindow, "Status : " + Result.Substring(4) + "\n", Color.Red);
+            }
+            else
+            {
+                // Need to send the password to the server
+                if (StatusCode != 230)
+                {
+                    FTPLoginSendPassword();
+                }
+                else
+                {
+                    Logged = true;
+                    WriteTextInLogWindow(logWindow, "Status : Connected to " + Server + "\n", Color.Red);
+                }
+            }
+        }
+
+        private void FTPLoginSendPassword()
+        {
+            SendCommand("PASS " + Password);
+
+            // 202 : the command is not implemented, superfluous at this site / password not required
+            // 230 : the user is logged in, proceed
+            if (!(StatusCode == 202 || StatusCode == 230))
+            {
+                FTPLogout();
+                WriteTextInLogWindow(logWindow, "Status : " + Result.Substring(4) + "\n", Color.Red);
+            }
+            else
+            {
+                Logged = true;
+                WriteTextInLogWindow(logWindow, "Status : Connected to " + Server + "\n", Color.Red);
+            }
+        }
+
+        private void ReadResponse()
+        {
+            StatusMessage = "";
+            Result = SplitResponse();
+            StatusCode = int.Parse(Result.Substring(0, 3));
+        }
+
+        private string SplitResponse()
+        {
+            string splitedResponse = ""; 
+
+            try
+            {
+                TranslateBytesIntoStatusMessage();
+
+                string[] msg = StatusMessage.Split('\n');
+                if (StatusMessage.Length > 2)
+                {
+                    // Remove Last \n
+                    StatusMessage = msg[msg.Length - 2];
+                }
+                else
+                {
+                    StatusMessage = msg[0];
+                }
+
+                for (int i = 0; i < msg.Length - 1; i++)
+                {
+                    WriteTextInLogWindow(logWindow, "Response : " + msg[i] + "\n", Color.Green);
+                }
+
+                splitedResponse = StatusMessage;
+            }
+            catch (Exception ex)
+            {
+                WriteTextInLogWindow(logWindow, "Status : ERROR. " + ex.Message + "\n", Color.Red);
+                FTPSocket.Close();
+            }
+
+            return splitedResponse;
+        }
+
+        private void TranslateBytesIntoStatusMessage()
+        {
+            // Count number of Bytes : socket lenght
+            Bytes = FTPSocket.Receive(Buffer, Buffer.Length, 0);
+            // Convert bytes into a string so we can understand the message
+            StatusMessage += Encoding.ASCII.GetString(Buffer, 0, Bytes);
+        }
+
+
+        private void SendCommand(string msg)
+        {
+            WriteTextInLogWindow(logWindow, "Command : " + msg + "\n", Color.Blue);
+            Byte[] CommandBytes = Encoding.ASCII.GetBytes((msg + "\r\n").ToCharArray());
+            FTPSocket.Send(CommandBytes, CommandBytes.Length, 0);
+            //read Response
+            ReadResponse();
+        }
+        #endregion
+
+        #region Close Connection Properly
+        private void CloseConnection()
+        {
+            WriteTextInLogWindow(logWindow, "Status : Closing Connection to " + Server + "\n", Color.Red);
+            if (FTPSocket != null)
+            {
+                SendCommand("QUIT");
+            }
+            FTPLogout();
+        }
+
+        private void FTPLogout()
+        {
+            if (FTPSocket != null)
+            {
+                FTPSocket.Close();
+                FTPSocket = null;
+            }
+            Logged = false;
+        }
+        #endregion
+
+        #region Log Window
+        private void WriteTextInLogWindow(RichTextBox box, string text, Color color)
+        {
+            // TODO : show message in a log window (rchLog)
+            // Like upper part in FileZila
+            MessageBox.Show(text);
+        }
+        #endregion
+    }
+}
